c5b5f1165f15df7f98ade31992064473eb24c2a8
Jose L. Velez <joselvelez@protonmail.com>
2021-11-09 10:21:57 -0600

    new footer design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,32 @@
-import { contractAddress } from "../constants/contractConstants";
+import { contractAddress, shortenAddress } from "../constants/contractConstants";
+import githubIcon from '../images/Octocat.jpg';
+import etherscanLogo from '../images/etherscan-logo-circle.png';
 
 export const Footer = () => {
+    const shortAddress = shortenAddress(contractAddress);
     return (
-        <div className="bg-white my-4 pr-8 pl-8 py-4 md:rounded flex flex-row justify-between">
-            <div>
-                Contract Address: &nbsp;
-                    <a href={`https://rinkeby.etherscan.io/address/${contractAddress}`}
-                    className="text-indigo-400 hover:text-indigo-500 transition ease-in-out duration-150" target="_blank" rel="noreferrer"
-                    >
-                        {contractAddress}
-                    </a>
+        <div className="bg-white my-4 pr-8 pl-8 py-4 md:rounded flex flex-row place-items-center justify-between">
+            <div className="flex flex-row place-items-center">
+                <img src={etherscanLogo} className="w-10 h-10 mr-3" alt="etherscan icon" />
+                <a href={`https://rinkeby.etherscan.io/address/${contractAddress}`}
+                className="text-indigo-400 hover:text-indigo-500 transition ease-in-out duration-150" target="_blank" rel="noreferrer"
+                >
+                    {shortAddress}
+                </a>
             </div>
-            <div>
-                Contract Code: <a href="https://github.com/joselvelez/nft_game"
-                 className="text-indigo-400 hover:text-indigo-500 transition ease-in-out duration-150" target="_blank" rel="noreferrer">Github Repo</a>
+            <div className="flex flex-row place-items-center">
+                <img src={githubIcon} className="w-10 h-10" alt="git hub icon" />
+                <a href="https://github.com/joselvelez/nft_game"
+                 className="text-indigo-400 hover:text-indigo-500 transition ease-in-out duration-150" target="_blank" rel="noreferrer">
+                     Contract Code
+                 </a>
             </div>
-            <div>
-                Front End Code: <a href="https://github.com/joselvelez/nft_game_client"
-                className="text-indigo-400 hover:text-indigo-500 transition ease-in-out duration-150" target="_blank" rel="noreferrer">Github Repo</a>
+            <div className="flex flex-row place-items-center">
+                <img src={githubIcon} className="w-10 h-10" alt="git hub icon" />
+                <a href="https://github.com/joselvelez/nft_game_client"
+                className="text-indigo-400 hover:text-indigo-500 transition ease-in-out duration-150" target="_blank" rel="noreferrer">
+                    Front End Code
+                </a>
             </div>
         </div>
     )
